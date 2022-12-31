7a5361f5753718cbc1ed44d24650a74debed37e8
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-12-31 18:29:32 +0100

    first commit

---
+++ src/main/java/org/sid/sqrsradar/SqrsRadarApplication.java
@@ -0,0 +1,13 @@
+package org.sid.sqrsradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqrsRadarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqrsRadarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/sqrsradar/SqrsRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.sqrsradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqrsRadarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
